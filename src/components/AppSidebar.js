5528c1e113474004c2ec901d395e41f0fe273055
torres-leo <leojts24@gmail.com>
2022-11-06 21:15:02 -0600

    add icon to group menu in sidebar

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -1,53 +1,46 @@
-import React from "react";
-import Logo from "../assets/brand/logo.jpeg";
-import { useSelector, useDispatch } from "react-redux";
+import React from 'react';
+import Logo from '../assets/brand/logo.jpeg';
+import { useSelector, useDispatch } from 'react-redux';
 
-import {
-  CSidebar,
-  CSidebarBrand,
-  CSidebarNav,
-  CSidebarToggler,
-} from "@coreui/react";
-import { AppSidebarNav } from "./AppSidebarNav";
+import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react';
+import { AppSidebarNav } from './AppSidebarNav';
 
-import SimpleBar from "simplebar-react";
-import "simplebar/dist/simplebar.min.css";
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 // sidebar nav config
-import navigation from "../_nav";
-import Image from "./Image";
+import navigation from '../_nav';
+import Image from './Image';
 
 const AppSidebar = () => {
-  const dispatch = useDispatch();
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable);
-  const sidebarShow = useSelector((state) => state.sidebarShow);
+	const dispatch = useDispatch();
+	const unfoldable = useSelector((state) => state.sidebarUnfoldable);
+	const sidebarShow = useSelector((state) => state.sidebarShow);
 
-  return (
-    <CSidebar
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: "set", sidebarShow: visible });
-      }}
-      className="Sidebar"
-    >
-      <CSidebarBrand className="Sidebar-brand" to="/">
-        <Image alt="logo" src={Logo} className="Sidebar-logo" />
-      </CSidebarBrand>
-      <CSidebarNav>
-        <SimpleBar>
-          <AppSidebarNav items={navigation} />
-        </SimpleBar>
-      </CSidebarNav>
-      <CSidebarToggler
-        className="d-none d-lg-flex"
-        onClick={() =>
-          dispatch({ type: "set", sidebarUnfoldable: !unfoldable })
-        }
-      />
-    </CSidebar>
-  );
+	return (
+		<CSidebar
+			position='fixed'
+			unfoldable={unfoldable}
+			visible={sidebarShow}
+			onVisibleChange={(visible) => {
+				dispatch({ type: 'set', sidebarShow: visible });
+			}}
+			className='Sidebar'
+		>
+			<CSidebarBrand className='Sidebar-brand' to='/'>
+				<Image alt='logo' src={Logo} className='Sidebar-logo' />
+			</CSidebarBrand>
+			<CSidebarNav>
+				<SimpleBar>
+					<AppSidebarNav items={navigation} />
+				</SimpleBar>
+			</CSidebarNav>
+			<CSidebarToggler
+				className='d-none d-lg-flex'
+				onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+			/>
+		</CSidebar>
+	);
 };
 
 export default React.memo(AppSidebar);
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,25 +1,25 @@
-import { Fragment } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AppSidebar from "../components/AppSidebar";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-import Docs from "../views/Docs";
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AppSidebar from '../components/AppSidebar';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import Docs from '../views/Docs';
 
 const Layout = () => {
-  return (
-    <Fragment>
-      <Router>
-        <Navbar />
-        <div>
-          <AppSidebar />
-          <Routes>
-            <Route path="/" element={<Docs />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Router>
+				<Navbar />
+				<div>
+					<AppSidebar />
+					<Routes>
+						<Route path='/' element={<Docs />} />
+					</Routes>
+					<Footer />
+				</div>
+			</Router>
+		</Fragment>
+	);
 };
 
 export default Layout;
