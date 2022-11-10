9469ff11d40463e98abc7a401a274c0c889f1952
Leo Torres <leojts24@gmail.com>
2022-11-10 15:54:11 -0600

    fixing sidebar

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
+import Icon from '../Icon';
 
 const Section = ({ children, title }) => {
   return (
     <div className='Section'>
-      <span className='Section-version'>Version: 2.1.0</span>
+      <div className='Section-breadcrumb'>
+        <div className='Section-icon'>
+          <Icon className="fa-solid fa-house" />
+        </div>
+        <span>
+          &gt;
+        </span>
+        <span>Advance Guides</span>
+      </div>
+      <p className='Section-version'>Version: 2.1.0</p>
       <h2 className='Section-title'>
         <span>{title}</span>
       </h2>
@@ -13,8 +23,8 @@ const Section = ({ children, title }) => {
 };
 
 Section.propTypes = {
-    title: PropTypes.string,
-    children: PropTypes.node
+  title: PropTypes.string,
+  children: PropTypes.node
 }
 
 export default Section;
